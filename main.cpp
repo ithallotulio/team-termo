7f311155599869d93244394944401ac76134c604
vkenzo2000 <33799668-vkenzo2000@users.noreply.replit.com>
2024-05-08 01:38:35 +0000

    Add functions escritaUser,limpavetor,verificador

--- main.cpp
+++ main.cpp
@@ -2,17 +2,25 @@
 #include <iostream>
 #include <string>
 #include <cctype>
-#include <ctime>
 #include <vector>
 #include <fstream>
+#include <chrono>    //pra add delay
+#include <thread>    //pra add delay
+#include <windows.h> // para a mudança de cor do texto
+#define TAMANHO 5
 
 using namespace std;
+using namespace std::this_thread;     // sleep_for, sleep_until
+using namespace std::chrono_literals; // ns, us, ms, s, h, etc.
 
 // Declaração de funções
 bool isAlpha(string str);
 bool isValidWord(string word);
 bool containsLetter(char letter, string word);
+void escritaUser(string userInput, int verificado[]);
 void stringUpper(string *str);
+void limpaVetor(int palavra[]);
+void verificador(string userInput, string randomWord, int verificado[]);
 string removeSpaces(string word);
 string getUserInput();
 string getRandomWord(vector<string>vecString);
@@ -21,17 +29,20 @@ vector<string> getFileInfo(string fileName);
 // Main
 int main() {
     // Declaração de variáveis
-    string userInput, randomWord;
     vector<string> vecWordList = getFileInfo("wordlist.txt");
+    string userWord, gameWord;
+    int verificado[TAMANHO] = {0};
 
     // Código
-    randomWord = getRandomWord(vecWordList);
+    gameWord = getRandomWord(vecWordList);
+    //gameWord = {"PAPAR"};
     cout << "Digite uma palavra: ";
-    userInput = getUserInput();
+    userWord = getUserInput();
+    verificador(userWord, gameWord, verificado);
 
-    cout << "A palavra sorteada foi: " << randomWord << endl;
-    cout << "Você digitou: " << userInput << endl;
-
+    cout << "\nVocê digitou: " << userWord << endl;
+    cout << "A palavra sorteada foi: " << gameWord << endl;
+    escritaUser(userWord, verificado);
     return 0;
 }
 
@@ -59,7 +70,7 @@ bool isValidWord(string word) {
     string vowels = {"AEIOU"};
     int i, vowelCount = 0;
 
-    if (!isAlpha(word) || word.length() != 5) 
+    if (!isAlpha(word) || word.length() != 5)
         return false;
 
     stringUpper(&word);
@@ -82,6 +93,29 @@ bool containsLetter(char letter, string word) {
     return false;
 }
 
+//escreve a resposta do user com as cores diferentes
+void escritaUser(string userInput, int verificado[]) {
+    int count=0;
+
+
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); //necessário para mudar as cores
+
+    cout << "---------------------" << endl;
+        for (count=0;count<TAMANHO;count++) {
+            cout << "|";
+            if (verificado[count]==2) {
+                SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+            } else if (verificado[count]==1) {
+                SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN);
+            }
+            cout << " " << userInput[count] << " " ;
+            SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED);
+            sleep_for(300ms);
+        }
+        cout << "|" << endl;
+        cout << "---------------------" << endl;
+}
+
 // Toda a string fica maiúscula
 void stringUpper(string *str) {
     for (char &c : *str) {
@@ -89,8 +123,37 @@ void stringUpper(string *str) {
     }
 }
 
+void limpaVetor(int palavra[]) {
+    for (int i = 0; i < TAMANHO; i++)
+        palavra[i]=0;
+}
+
+void verificador(string userWord, string gameWord, int verificado[]) {
+    int countpalavra=0;
+    int counttermo=0;
+
+    limpaVetor(verificado);
+
+    for(countpalavra=0;countpalavra<TAMANHO;countpalavra++) {
+        if(userWord[countpalavra]==gameWord[countpalavra]) {
+            verificado[countpalavra] = 2;
+        }
+    }
+
+    for(counttermo=0;counttermo<TAMANHO;counttermo++) {
+        if(verificado[counttermo]!=2) {
+            for(countpalavra=0;countpalavra<TAMANHO;countpalavra++) {
+                if(verificado[countpalavra]==0 && userWord[countpalavra]==gameWord[counttermo]) {
+                    verificado[countpalavra] = 1;
+                    break;
+                }
+            }
+        }
+    }
+}
+
 // Retorna a palavra sem espaços
-string removeSpaces(string word){
+string removeSpaces(string word) {
     string wordNoSpaces;
     for (char c: word) {
         if (c != ' ') {
@@ -103,9 +166,9 @@ string removeSpaces(string word){
 /*
 * Coleta e retorna a entrada do usuário após validação
 * Remove espaços da entrada
-* Retorna a string maiúscula 
+* Retorna a string maiúscula
 */
-string getUserInput(){
+string getUserInput() {
     string input;
 
     getline(cin, input);
@@ -119,19 +182,14 @@ string getUserInput(){
 }
 
 // Retorna uma palavra aleatória de um vetor de palavras
-string getRandomWord(vector<string>vecString){
+string getRandomWord(vector<string>vecString) {
     srand(time(0));
     int RandomIndex = rand() % vecString.size();
     return vecString[RandomIndex];
 }
 
-/* 
-* Retorna um vetor de string, tendo em cada índice uma linha do fileName.txt
-* Indice 0 armazena 1º linha 
-* Indice 1 armazena 2º linha
-* Indice 2 armazena 3º linha, e assim por diante...
-*/
-vector<string> getFileInfo(string fileName){
+// Retorna um vetor de string, onde cada index do vetor armazena uma linha do arquivo.txt
+vector<string> getFileInfo(string fileName) {
     ifstream file(fileName);
     vector<string> fileInfo;
     string line;
@@ -140,4 +198,3 @@ vector<string> getFileInfo(string fileName){
     }
     return fileInfo;
 }
-
