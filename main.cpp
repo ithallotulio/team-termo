9f0647cec9480fbf197da1479eaec75df6cbb8a3
Ithallo Tulio <51161234+ithallotulio@users.noreply.github.com>
2024-05-08 04:35:18 -0300

    Add function showGameScreen
    Adicionado a função showGameScreen e alterado suas dependências.
    
    Dependências alteradas:
    - Renomeado função verificador() para checkUserWord()
    - Renomeado função escritaUser() para showUserWordChecked() e add
    parâmetro delay
    - Renomeado variável int verificado[] para userWordChecked[]
    
    Outros:
    - Add variável vecUserWord

--- main.cpp
+++ main.cpp
@@ -7,20 +7,23 @@
 #include <chrono>    //pra add delay
 #include <thread>    //pra add delay
 #include <windows.h> // para a mudança de cor do texto
+#include <cstdlib>
+
 #define TAMANHO 5
 
 using namespace std;
 using namespace std::this_thread;     // sleep_for, sleep_until
-using namespace std::chrono_literals; // ns, us, ms, s, h, etc.
+using namespace std::chrono;         // miliseconds
 
 // Declaração de funções
 bool isAlpha(string str);
 bool isValidWord(string word);
 bool containsLetter(char letter, string word);
-void escritaUser(string userInput, int verificado[]);
+void showUserWordChecked(string userInput, int userWordChecked[], int delay);
 void stringUpper(string *str);
 void limpaVetor(int palavra[]);
-void verificador(string userInput, string randomWord, int verificado[]);
+void checkUserWord(string userInput, string randomWord, int userWordChecked[]);
+void showGameScreen(vector<string> vecUserWord, string gameWord, int userWordChecked[]);
 string removeSpaces(string word);
 string getUserInput();
 string getRandomWord(vector<string>vecString);
@@ -30,19 +33,19 @@ vector<string> getFileInfo(string fileName);
 int main() {
     // Declaração de variáveis
     vector<string> vecWordList = getFileInfo("wordlist.txt");
+    vector<string> vecUserWord;
     string userWord, gameWord;
-    int verificado[TAMANHO] = {0};
+    int userWordChecked[TAMANHO] = {0};
 
     // Código
     gameWord = getRandomWord(vecWordList);
     //gameWord = {"PAPAR"};
-    cout << "Digite uma palavra: ";
-    userWord = getUserInput();
-    verificador(userWord, gameWord, verificado);
-
-    cout << "\nVocê digitou: " << userWord << endl;
-    cout << "A palavra sorteada foi: " << gameWord << endl;
-    escritaUser(userWord, verificado);
+    while (1) {
+        cout << "Digite uma palavra: ";
+        userWord = getUserInput();
+        vecUserWord.push_back(userWord);
+        showGameScreen(vecUserWord, gameWord, userWordChecked);
+    }
     return 0;
 }
 
@@ -93,29 +96,6 @@ bool containsLetter(char letter, string word) {
     return false;
 }
 
-//escreve a resposta do user com as cores diferentes
-void escritaUser(string userInput, int verificado[]) {
-    int count=0;
-
-
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); //necessário para mudar as cores
-
-    cout << "---------------------" << endl;
-        for (count=0;count<TAMANHO;count++) {
-            cout << "|";
-            if (verificado[count]==2) {
-                SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-            } else if (verificado[count]==1) {
-                SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN);
-            }
-            cout << " " << userInput[count] << " " ;
-            SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED);
-            sleep_for(300ms);
-        }
-        cout << "|" << endl;
-        cout << "---------------------" << endl;
-}
-
 // Toda a string fica maiúscula
 void stringUpper(string *str) {
     for (char &c : *str) {
@@ -128,28 +108,51 @@ void limpaVetor(int palavra[]) {
         palavra[i]=0;
 }
 
-void verificador(string userWord, string gameWord, int verificado[]) {
+void checkUserWord(string userWord, string gameWord, int userWordChecked[]) {
     int countpalavra=0;
     int counttermo=0;
 
-    limpaVetor(verificado);
+    limpaVetor(userWordChecked);
 
     for(countpalavra=0;countpalavra<TAMANHO;countpalavra++) {
         if(userWord[countpalavra]==gameWord[countpalavra]) {
-            verificado[countpalavra] = 2;
+            userWordChecked[countpalavra] = 2;
         }
     }
 
     for(counttermo=0;counttermo<TAMANHO;counttermo++) {
-        if(verificado[counttermo]!=2) {
+        if(userWordChecked[counttermo]!=2) {
             for(countpalavra=0;countpalavra<TAMANHO;countpalavra++) {
-                if(verificado[countpalavra]==0 && userWord[countpalavra]==gameWord[counttermo]) {
-                    verificado[countpalavra] = 1;
+                if(userWordChecked[countpalavra]==0 && userWord[countpalavra]==gameWord[counttermo]) {
+                    userWordChecked[countpalavra] = 1;
                     break;
                 }
             }
         }
     }
+}
+
+//escreve a resposta do user com as cores diferentes
+void showUserWordChecked(string userInput, int userWordChecked[], int delay = 0) {
+    int count=0;
+
+
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); //necessário para mudar as cores
+
+    cout << "---------------------" << endl;
+        for (count=0;count<TAMANHO;count++) {
+            sleep_for(milliseconds(delay));
+            cout << "|";
+            if (userWordChecked[count]==2) {
+                SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+            } else if (userWordChecked[count]==1) {
+                SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN);
+            }
+            cout << " " << userInput[count] << " " ;
+            SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED);
+        }
+        cout << "|" << endl;
+        cout << "---------------------" << endl;
 }
 
 // Retorna a palavra sem espaços
@@ -197,4 +200,22 @@ vector<string> getFileInfo(string fileName) {
         fileInfo.push_back(line);
     }
     return fileInfo;
+}
+
+void showGameScreen(vector<string> vecUserWord, string gameWord, int userWordChecked[]) {
+    int i;
+    system("cls");
+    for (i=0; i < vecUserWord.size()-1; i++){
+        checkUserWord(vecUserWord[i], gameWord, userWordChecked);
+        showUserWordChecked(vecUserWord[i], userWordChecked);
+    }
+    checkUserWord(vecUserWord[i], gameWord, userWordChecked);
+    showUserWordChecked(vecUserWord[i], userWordChecked, 300);
+    if (vecUserWord.size() < 6){
+        for (i=0; i < 6-vecUserWord.size(); i++){
+            cout << "---------------------" << endl;
+            cout << "|   |   |   |   |   |" << endl;
+            cout << "---------------------" << endl;
+        }
+    }
 }
