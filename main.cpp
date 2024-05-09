e96e50e4d44841eb97edfb5129d1b58f6681e5c0
Ithallo Tulio <51161234+ithallotulio@users.noreply.github.com>
2024-05-09 18:21:56 +0000

    Code Reorganization (main.cpp)
    Moved functions to more appropriate locations, structured the code, and add separator comments.

--- main.cpp
+++ main.cpp
@@ -15,22 +15,35 @@ using namespace std;
 using namespace std::this_thread;     // sleep_for, sleep_until
 using namespace std::chrono;         // miliseconds
 
-// Declaração de funções
+/* --------------------------------------------------------------------- */
+/*                         Declaração de funções                         */
+/* --------------------------------------------------------------------- */
+
+// Utility
 bool isAlpha(string str);
-bool isValidWord(string word);
 bool containsLetter(char letter, string word);
-bool isComplete(int userWordChecked[TAMANHO]);
-void showUserWordChecked(string userInput, int userWordChecked[], int delay);
 void stringUpper(string *str);
 void zeroArray(int array[], int size);
-void checkUserWord(string userInput, string randomWord, int userWordChecked[]);
-void showGameScreen(vector<string> vecUserWord, string gameWord, int userWordChecked[]);
 string removeSpaces(string word);
+
+// Game Mechanics and rules
+void checkUserWord(string userInput, string randomWord, int userWordChecked[]);
+bool isComplete(int userWordChecked[TAMANHO]);
+bool isValidWord(string word);
 string getUserInput();
 string getRandomWord(vector<string>vecString);
+
+// Game Interface
+void showUserWordChecked(string userInput, int userWordChecked[], int delay);
+void showGameScreen(vector<string> vecUserWord, string gameWord, int userWordChecked[]);
+
+// File
 vector<string> getFileInfo(string fileName);
 
-// Main
+/* --------------------------------------------------------------------- */
+/*                                 Main                                  */
+/* --------------------------------------------------------------------- */
+
 int main() {
     // Declaração de variáveis
     vector<string> vecWordList = getFileInfo("wordlist.txt");
@@ -59,40 +72,17 @@ int main() {
     return 0;
 }
 
-// Funções
+/* --------------------------------------------------------------------- */
+/*                                Funções                                */
+/* --------------------------------------------------------------------- */
+
+//                  ==========   Utility   ==========
 
 // Verifica se uma string contém apenas char alfabéticos (alfabeto inglês)
 bool isAlpha(string str) {
     for (char c : str) {
         if (!isalpha(c))
             return false;
-    }
-    return true;
-}
-
-/*
- * Verifica se uma palavra é válida, retornando:
- * Falso, char não alfabetico
- * Falso, tem espaço
- * Falso, não tem 5 letras
- * Falso, todas letras vogais (5 vogais)
- * Falso, todas letras consoantes (0 vogais)
- * Verdadeiro, qualquer outra situação
- */
-bool isValidWord(string word) {
-    string vowels = {"AEIOU"};
-    int i, vowelCount = 0;
-
-    if (!isAlpha(word) || word.length() != 5)
-        return false;
-
-    stringUpper(&word);
-    for (i = 0; i < 5; i++) {
-        if (containsLetter(word[i], vowels))
-            vowelCount++;
-    }
-    if (vowelCount == 5 || vowelCount == 0) {
-        return false;
     }
     return true;
 }
@@ -104,19 +94,6 @@ bool containsLetter(char letter, string word) {
             return true;
     }
     return false;
-}
-
-bool isComplete(int userWordChecked[TAMANHO]){
-    bool check;
-    for (int i = 0;i<TAMANHO;i++){
-        if( userWordChecked[i] == 2 ){
-            check = true;
-        }else{
-            check = false;
-            break;
-        }
-    }
-    return check;
 }
 
 // Toda a string fica maiúscula
@@ -131,6 +108,19 @@ void zeroArray(int array[], int size = 5) {
     for (int i = 0; i < size; i++)
         array[i]=0;
 }
+
+// Retorna a palavra sem espaços
+string removeSpaces(string word) {
+    string wordNoSpaces;
+    for (char c: word) {
+        if (c != ' ') {
+            wordNoSpaces += c;
+        }
+    }
+    return wordNoSpaces;
+}
+
+//            ==========   Game Mechanics and rules   ==========
 
 /*
 * Verifica o quão parecida é a palavra digitada pelo usuário com a palavra do jogo
@@ -164,38 +154,44 @@ void checkUserWord(string userWord, string gameWord, int userWordChecked[]) {
     }
 }
 
-//escreve a resposta do user com as cores diferentes
-void showUserWordChecked(string userInput, int userWordChecked[], int delay = 0) {
-    int count=0;
-
-
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); //necessário para mudar as cores
-
-    cout << "---------------------" << endl;
-        for (count=0;count<TAMANHO;count++) {
-            sleep_for(milliseconds(delay));
-            cout << "|";
-            if (userWordChecked[count]==2) {
-                SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-            } else if (userWordChecked[count]==1) {
-                SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN);
-            }
-            cout << " " << userInput[count] << " " ;
-            SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED);
-        }
-        cout << "|" << endl;
-        cout << "---------------------" << endl;
-}
-
-// Retorna a palavra sem espaços
-string removeSpaces(string word) {
-    string wordNoSpaces;
-    for (char c: word) {
-        if (c != ' ') {
-            wordNoSpaces += c;
+bool isComplete(int userWordChecked[TAMANHO]) {
+    bool check;
+    for (int i = 0;i<TAMANHO;i++) {
+        if (userWordChecked[i] == 2) {
+            check = true;
+        } else {
+            check = false;
+            break;
         }
     }
-    return wordNoSpaces;
+    return check;
+}
+
+/*
+ * Verifica se uma palavra é válida, retornando:
+ * Falso, char não alfabetico
+ * Falso, tem espaço
+ * Falso, não tem 5 letras
+ * Falso, todas letras vogais (5 vogais)
+ * Falso, todas letras consoantes (0 vogais)
+ * Verdadeiro, qualquer outra situação
+ */
+bool isValidWord(string word) {
+    string vowels = {"AEIOU"};
+    int i, vowelCount = 0;
+
+    if (!isAlpha(word) || word.length() != 5)
+        return false;
+
+    stringUpper(&word);
+    for (i = 0; i < 5; i++) {
+        if (containsLetter(word[i], vowels))
+            vowelCount++;
+    }
+    if (vowelCount == 5 || vowelCount == 0) {
+        return false;
+    }
+    return true;
 }
 
 /*
@@ -223,15 +219,27 @@ string getRandomWord(vector<string>vecString) {
     return vecString[RandomIndex];
 }
 
-// Retorna um vetor de string, onde cada index do vetor armazena uma linha do arquivo.txt
-vector<string> getFileInfo(string fileName) {
-    ifstream file(fileName);
-    vector<string> fileInfo;
-    string line;
-    while(getline(file, line)){
-        fileInfo.push_back(line);
+//                  ==========   Game Interface   ==========
+
+// Escreve a resposta do user com as cores diferentes
+void showUserWordChecked(string userInput, int userWordChecked[], int delay = 0) {
+    int count=0;
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); // necessário para mudar as cores
+
+    cout << "---------------------" << endl;
+    for (count=0;count<TAMANHO;count++) {
+        sleep_for(milliseconds(delay));
+        cout << "|";
+        if (userWordChecked[count]==2) {
+            SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+        } else if (userWordChecked[count]==1) {
+            SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN);
+        }
+        cout << " " << userInput[count] << " " ;
+        SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED);
     }
-    return fileInfo;
+    cout << "|" << endl;
+    cout << "---------------------" << endl;
 }
 
 void showGameScreen(vector<string> vecUserWord, string gameWord, int userWordChecked[]) {
@@ -251,3 +259,18 @@ void showGameScreen(vector<string> vecUserWord, string gameWord, int userWordChe
         }
     }
 }
+
+
+//                      ==========   File   ==========
+
+// Retorna um vetor de string, onde cada index do vetor armazena uma linha do arquivo.txt
+vector<string> getFileInfo(string fileName) {
+    ifstream file(fileName);
+    vector<string> fileInfo;
+    string line;
+    while(getline(file, line)){
+        fileInfo.push_back(line);
+    }
+    return fileInfo;
+}
+
